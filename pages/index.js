eff3dc353d1ef73e207429078222b299e2903996
ivan fung <ivan122192@gmail.com>
2023-02-28 20:08:45 +0800

    change ui

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link'
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, onValue, set, get,child} from "firebase/database";
+import { getDatabase, ref, onValue, set, get,child,serverTimestamp} from "firebase/database";
 import {useState, useEffect} from "react"
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/router'
@@ -14,6 +13,11 @@ import Button from '@mui/material/Button';
 import CloseIcon from '@mui/icons-material/Close';
 import TextField from '@mui/material/TextField';
 
+import { createTheme,ThemeProvider } from '@mui/material/styles';
+import { Typography, AppBar, Card, CardActions,
+  CardContent,CssBaseline,Grid,Toolbar,Container } from "@mui/material";
+
+import RestaurantIcon from '@mui/icons-material/Restaurant';
 
 export default function Home() {
   
@@ -21,15 +25,13 @@ export default function Home() {
   const [phoneNumber, setPhoneNumer] = useState("")
   const [pplNumber,setPplNumber] = useState("")
   const [showAlert,setShowAlert] = useState(false)
-
   const router = useRouter()
- 
-
   const searchParams = useSearchParams();
-
   const key = router.query.key;
 
+  
   useEffect(()=>{
+    //document.body.style.backgroundColor = "#D0D0D0"
     const db = getDatabase();
 
     if (router.isReady){
@@ -43,50 +45,68 @@ export default function Home() {
     })
   }
 
-
   },[router.isReady])
 
   const pad =(num, size) =>{
     num = num.toString();
     while (num.length < size) num = "0" + num;
     return num;
-}
+  }
+  const isNumeric =(value)=> {
+      return /^\d+$/.test(value);
+  }
 
   const onClickSubmit =()=>{
-    setData(phoneNumber)
-    const db = getDatabase();
-
-    get(child(ref(db),"key/"+key)).then((snapshot)=>{
-      if (snapshot.exists()){
-        
-        get(child(ref(db),"count/"+key)).then((snapshot)=>{
-          if (snapshot.exists()){
-          let ticket = "A" + pad(snapshot.val(),3)
-          
-          let changedCount = snapshot.val()+1
-          set(ref(db, 'count/' + key), changedCount)
-          set(ref(db, 'queue/' + key +"/"+ phoneNumber), {
-            numPeople: pplNumber
-          })
-
-          router.push("/posts/first-post/?key=" + phoneNumber +"&code=" + key + "&ticket=" + ticket)
-          }
-
-        })
-        
-
-      }else{
-        setShowAlert(true)
-      }
-    })
    
-    setPhoneNumer("")
+    const db = getDatabase();
+    var phoneOk = false
+    var numPplOk = false
+
+    if(phoneNumber.length === 8 && isNumeric(phoneNumber)){
+      phoneOk = true
+
+      }
+    if(isNumeric(pplNumber)){
+      numPplOk = true
+    }
+
+    if (phoneOk && numPplOk){
+
+        get(child(ref(db),"key/"+key)).then((snapshot)=>{
+          if (snapshot.exists()){
+            
+              get(child(ref(db),"count/"+key)).then((snapshot)=>{
+                if (snapshot.exists()){
+                let ticket = "A" + pad(snapshot.val(),3)
+                
+                let changedCount = snapshot.val()+1
+                set(ref(db, 'count/' + key), changedCount)
+                set(ref(db, 'queue/' + key +"/"+ phoneNumber), {
+                  numPeople: pplNumber,
+                  ticketNumber:snapshot.val(),
+                  ticketCode: ticket,
+                  createdAt: serverTimestamp()
+                })
+
+                router.push("/posts/first-post/?key=" + phoneNumber +"&code=" + key)
+                }
+              })
+              
+            }else{
+              setShowAlert(true)
+            }
+          })
+        
+          setPhoneNumer("")
+          setPplNumber("")
+    }else{
+      setShowAlert(true)
+    }
   }
   const alert = () =>{
     if(showAlert){
       return(
         
-
         <Alert
          severity="error"
           action={
@@ -103,19 +123,45 @@ export default function Home() {
           }
           sx={{ mb: 2, position:"absolute", bottom:0, width:"90%"}}
         >
-          key error
+          輸入資料不正確
         </Alert>
-
-        
-       
-
       )
     }
   }
+  const onPressNumButton = (num) =>{
+    setPplNumber(num)
 
+  }
+  const theme = createTheme({
+    typography: {
+
+      button: { 
+        fontSize: 15,
+        fontWeight: 700,
+      },    
+    },  
+    status: {
+      danger: '#e53e3e',
+    },
+    palette: {
+      primary: {
+        main: '#0971f1',
+        darker: '#053e85',
+      },
+      neutral: {
+        main: '#64748B',
+        contrastText: '#fff',
+      },
+      secondary:{
+        main: '#AFCBF5',
+        contrastText: '#0052CE',
+
+      }
+    },
+  });
 
   return (
-    <div className={styles.container}>
+    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
       <Head>
         <title>testApp</title>
         <link rel="icon" href="/favicon.ico" />
@@ -123,92 +169,119 @@ export default function Home() {
 
       <main>
 
-       
-       
-
         <Box 
-        sx={{ p: 5, display:'flex',flexDirection:"column",gap: 2, boxShadow:"3px 3px 20px rgba(0,0,0,0.2)", borderRadius:5, alignItems:"center"}}>
-           <h1 >{data}</h1>
-          <div style = {{display:"flex",alignItems:"center"}}>
-          <text>電話:</text>
-          <TextField sx = {{marginLeft:2}}
-                    value = {phoneNumber}
-                    onChange={(event)=> setPhoneNumer(event.target.value)}
-                    />
+        sx={{ p: 1, display:'flex',flexDirection:"column",
+        gap: 2, boxShadow:"3px 3px 20px rgba(0,0,0,0.0)",
+         borderRadius:2, alignItems:"center", margin:5, 
+         backgroundColor:"white", maxWidth:400}}>
+          
+          
+          <div style = {{display:'flex', flexDirection:"row"}}>
+          <Typography sx={{fontWeight:"700"}}>歡迎來到</Typography>
           </div>
           
-          <div style = {{display:"flex",alignItems:"center"}}>
-          <text>人數:</text>
-          <TextField sx = {{marginLeft:2}}
+           <Typography  color='primary' variant='h4' align='center'
+           sx={{fontWeight:"700"}}
+           
+           >{data}</Typography>
+
+
+          <div style = {{marginTop:30, display:"flex",alignItems:"center",width:"90%",flexDirection:"column"}}>
+          <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="phone"
+              label="電話"
+              name="phone"
+              autoComplete="phone"
+              autoFocus
+              value = {phoneNumber}
+              onChange={(event)=> setPhoneNumer(event.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="number"
+              label="人數"
+              name="phone"
+              autoFocus
+              value = {pplNumber}
+              onChange={(event)=> setPplNumber(event.target.value)}
+            />
+          {/* <TextField sx = {{marginLeft:2, width:"100%"}}
+                    value = {phoneNumber}
+                    onChange={(event)=> setPhoneNumer(event.target.value)}
+                    /> */}
+          </div>
+          
+          {/* <div style = {{display:"flex",alignItems:"center", width:"90%",marginBottom:20 }}>
+          <Typography variant='h6' sx={{width:70, fontWeight:"700"}}>人數:</Typography>
+          <TextField sx = {{marginLeft:2, width:"100%"}}
                     value = {pplNumber}
                     onChange={(event)=> setPplNumber(event.target.value)}
                     />
 
-          </div>
-          <Button sx = {{p:2,width:"100%"}}
+          </div> */}
+          <ThemeProvider theme={theme}>
+          <div style = {{width:"90%", marginBottom:10}}>
+
+          <Grid  container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+              <Grid item  xs={1} sm={2} md={4}>
+                <Button  variant="outlined" fullWidth 
+                onClick={()=>onPressNumButton(1)}>1人</Button>    
+              </Grid>
+              
+              <Grid item  xs={1} sm={2} md={4}>
+                <Button variant="outlined" fullWidth 
+                onClick={()=>onPressNumButton(2)}>2人</Button>       
+              </Grid>
+              
+              <Grid item  xs={1} sm={2} md={4}>
+                <Button variant="outlined" fullWidth 
+                onClick={()=>onPressNumButton(3)}>3人</Button> 
+              </Grid>
+              
+              <Grid item  xs={1} sm={2} md={4}>
+                <Button variant="outlined" fullWidth 
+                onClick={()=>onPressNumButton(4)}>4人</Button> 
+              </Grid>
+
+              <Grid item  xs={1} sm={2} md={4}>
+                <Button variant="outlined" fullWidth 
+                onClick={()=>onPressNumButton(5)}>5人</Button> 
+              </Grid>
+
+              <Grid item  xs={1} sm={2} md={4}>
+                <Button variant="outlined" fullWidth 
+                onClick={()=>onPressNumButton(6)}>6人</Button> 
+              </Grid>
+              <Grid item  xs={1} sm={2} md={4}>
+                <Button variant="outlined" fullWidth 
+                onClick={()=>onPressNumButton(7)}>7人</Button> 
+              </Grid>
+              <Grid item  xs={1} sm={2} md={4}>
+                <Button variant="outlined" fullWidth 
+                onClick={()=>onPressNumButton(8)}>8人</Button> 
+              </Grid>
+            </Grid>
+            </div>
+          </ThemeProvider>
+
+          <Button sx = {{p:2, width:"90%", fontWeight:"700", fontSize:20}}
             variant="contained"
             onClick={()=>onClickSubmit()}>
-              開始排隊
+              <RestaurantIcon sx={{mr:2}}/>
+              test
         </Button>
 
         </Box>
 
         {alert()}
-      
-      
+           
       </main>
+      </div>
 
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+      )
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import Image from 'next/image';
 import Head from 'next/head';
 import Script from 'next/script';
-import Layout from '../../components/layout'
 import { getDatabase, ref, onValue, set, get,child} from "firebase/database";
 import {useState, useEffect} from "react"
 import { useSearchParams } from 'next/navigation';
@@ -12,6 +11,11 @@ import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import CloseIcon from '@mui/icons-material/Close';
 import TextField from '@mui/material/TextField';
+import { PhotoCamera } from "@mui/icons-material";
+
+import { Typography, AppBar, Card, CardActions,
+  CardContent,CssBaseline,Grid,Toolbar,Container } from "@mui/material";
+import { color } from "@mui/system";
 
 
 export default function FirstPost() {
@@ -24,25 +28,32 @@ export default function FirstPost() {
     const [code,setCode] = useState("")
     const [phoneNumber,setPhoneNumer] = useState("")
     const [ticket,setTicket] = useState("")
+    const [ticketIndex, setTicketIndex] = useState("")
+    const [lastTicket,setLastTicket] = useState("")
+    const [currentTicket,setCurrentTicket] = useState("")
+    const [queue,setQueue] = useState([])
+    const [tableRemain, setTableRemain] = useState(0)
+
+    const db = getDatabase()
 
     console.log('run page')
     useEffect(()=>{
-        console.log('run effect')
         
         if (router.isReady){
         const key2 = router.query.key
         const code = router.query.code
-        const ticket = router.query.ticket
+
         console.log('have key')
         console.log(code)
-        const db = getDatabase()
+        
         get(child(ref(db),"queue/"+code+"/"+key2)).then((snapshot)=>{
             if (snapshot.exists()){
                 const data = snapshot.val()
                 setnumppl(data.numPeople)
                 setCode(code)
                 setPhoneNumer(key2)
-                setTicket(ticket)
+                setTicket(data.ticketCode)
+                setTicketIndex(data.ticketNumber)
 
             }
         })
@@ -51,33 +62,95 @@ export default function FirstPost() {
 
     },[router.isReady])
 
-    return (
-        
+
+    useEffect(()=>{
+     
+      
+      const dbRef = ref(db,"queue/" + router.query.code)
+
+      onValue(dbRef,(snapshot)=>{
+
+        var tempArr = []
+        snapshot.forEach((data) =>{
+          let ticketCode = data.val().ticketCode
+          let ticketNumber = data.val().ticketNumber
+          tempArr.push({"ticketCode":ticketCode,"ticketNumber":ticketNumber})
     
-        <Layout>
-        <Head>
-            <title>post</title>
-        </Head>
+        })
+        tempArr.sort((a,b) => a.ticketNumber-b.ticketNumber)
+        //
+
+        if (tempArr.length > 0){
+          setQueue(tempArr)
+          console.log(tempArr[tempArr.length-1])
+        }
         
-        
-        <Box 
-        sx={{ p: 5, display:'flex',
-                flexDirection:"column",
-                gap: 2, 
-                boxShadow:"3px 3px 20px rgba(0,0,0,0.2)", 
-                borderRadius:5, alignItems:"center"}}>
-        
-        <h1>{'票號 ' + ticket}</h1>
-        <h1>{'電話 ' + phoneNumber}</h1>
-        <h1>{numppl} 人</h1>
+
+      })
+
+    },[router.isReady])
+
+    useEffect(()=>{
+      const dbRef = ref(db,"caller/" + router.query.code + "/A/ticketNum")
+
+      onValue(dbRef,(snapshot)=>{
+        let data = snapshot.val()
+        setCurrentTicket(data)
+
+      })
+
+    },[router.isReady])
+
+    useEffect(()=>{
+      let myTicket = ticket
+      let length = queue.length
+      var myIndex = 0
+      var currentIndex = 0
+      if (length>0){
+        for (let i =0; i<length ; i++){
+          if (queue[i].ticketCode === myTicket){
+            myIndex = i
+          }else if (queue[i].ticketCode === currentTicket){
+            currentIndex = i
+          }
+        }
+        setTableRemain(myIndex-currentIndex)
+      }
+    },[currentTicket,queue])
+
+   
+
+    return (
+      <>
+        <CssBaseline/>
+      
+        <main>
+          <div>
+            <Container maxWidth = "m" sx = {{backgroundColor:'#ECECEC',padding:2, display: "flex", flexDirection:"column"}}>
+              <Card sx = {{marginTop:2,boxShadow:3, minHeight:50}}>
+                <Typography variant="h4" align="center" color="black">{ticket}</Typography>
+              </Card>
+              <div style={{flexDirection:"row", display:"flex", gap:20}}>
+                
+                <Card sx = {{marginTop:2, flex:1,boxShadow:3,minHeight:50}}>
+                  <Typography variant="h6" align="center" color="black">{currentTicket}</Typography>
+                </Card>
+                <Card sx = {{marginTop:2, flex:1,boxShadow:3,minHeight:50}}>
+                  <Typography variant="h6" align="center" color="black">{tableRemain}</Typography>
+                </Card>
+              </div>
+            </Container>
+            <div style={{height:2, backgroundColor:"black"}}/>
+
+          </div>
+        </main>
+      
+      
+      </>
        
-        <h2>
-           
-
-
-        </h2>
-        </Box>
-        </Layout>
+        
+        
+ 
         
 
     );
